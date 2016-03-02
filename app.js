20527408f6743fb3f6ffc4972682a9a48448afd2
Mike Ulvila <mikeulvila@me.com>
2016-03-02 08:09:17 -0600

    socket.io tutorial version

---
+++ app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+
+});
+
+http.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
